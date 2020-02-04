f694c21963a418211d1655f3643c410c2ee77326
jessicarush <hello@cyan.red>
2020-02-04 14:42:03 -0800

    Docs: minor improvements

--- css_transitions/script.js
+++ css_transitions/script.js
@@ -1,22 +1,24 @@
 function randomBg() {
+
   // Generate a random number between 0 and given number (inclusive)
   function random(n) {
     return Math.floor(Math.random() * (n + 1));
   }
+
   // Generate a random rgb color
   function randomRGB() {
-    let rgbColor = 'rgb(' + random(255) + ',' + random(255) + ',' + random(255) + ')';
+    let rgbColor = `rgb(${random(255)}, ${random(255)}, ${random(255)})`;
     return rgbColor;
   }
+
   // Set an elements --random-bg property to a random color
-  function changeBg() {
-    let els = document.querySelectorAll('.color-box');
-    for (el of els) {
+  function changeBg(selector) {
+    let els = document.querySelectorAll(selector);
+    for (let el of els) {
       el.style.setProperty('--random-bg', randomRGB());
     }
-
   }
-  changeBg();
+  changeBg('.color-box');
 }
 
 randomBg();
